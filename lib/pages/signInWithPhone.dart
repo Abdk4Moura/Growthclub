c448607e807f407f743c01c2c2ed3ddea5b91e70
Abdulkabir Agboola <pro.kaiserlautern@gmail.com>
2022-08-11 16:59:14 +0100

    Some consistent designs have been added
    (cherry picked from commit 33edded77f686414b78e5f143da8719059a59520)

---
+++ lib/pages/signInWithPhone.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+
+import 'package:growthclub/auth/phoneSignIn.dart';
+
+import '../auth/emailAnd.dart';
+import '../growthron_ui.dart';
+
+class SignInWithPhoneWidget extends StatefulWidget {
+  const SignInWithPhoneWidget({Key? key}) : super(key: key);
+
+  @override
+  _SignInWithPhoneWidgetState createState() => _SignInWithPhoneWidgetState();
+}
+
+class _SignInWithPhoneWidgetState extends State<SignInWithPhoneWidget> {
+  TextEditingController? emailAddressController;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    emailAddressController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          backgroundColor: Color(0xFFF1F4F8),
+          automaticallyImplyLeading: false,
+          flexibleSpace: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                      child: GrowthronIconButton(
+                        borderColor: Colors.transparent,
+                        borderRadius: 30,
+                        borderWidth: 1,
+                        buttonSize: 50,
+                        icon: Icon(
+                          Icons.arrow_back_rounded,
+                          color: Color(0xFF0F1113),
+                          size: 24,
+                        ),
+                        onPressed: () async {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(4, 0, 0, 0),
+                      child: Text(
+                        'Back',
+                        style: Theme.of(context).textTheme.titleLarge?.apply(
+                              color: Color(0xFF0F1113),
+                            ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(24, 0, 0, 0),
+                child: Text(
+                  'Sign in with phone',
+                  style: Theme.of(context).textTheme.titleLarge?.apply(
+                        fontFamily: 'Outfit',
+                        color: Color(0xFF0F1113),
+                      ),
+                ),
+              ),
+            ],
+          ),
+          actions: [],
+          elevation: 0,
+        ),
+      ),
+      backgroundColor: Color(0xFFF1F4F8),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(20, 4, 20, 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(4, 4, 4, 4),
+                    child: Text(
+                      'We will send you a verification code to this number, please enter the phone number associated with your account below.',
+                      style: Theme.of(context).textTheme.bodyText2?.apply(
+                            fontFamily: 'Outfit',
+                            color: Color(0xFF57636C),
+                          ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(24, 4, 24, 0),
+            child: Container(
+              width: double.infinity,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 5,
+                    color: Color(0x4D101213),
+                    offset: Offset(0, 2),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: TextFormField(
+                controller: emailAddressController,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: 'Phone number',
+                  labelStyle: Theme.of(context).textTheme.bodyText2?.apply(
+                        fontFamily: 'Outfit',
+                        color: Color(0xFF57636C),
+                      ),
+                  hintText: 'Enter your phone...',
+                  hintStyle: Theme.of(context).textTheme.bodyText1?.apply(
+                        fontFamily: 'Lexend Deca',
+                        color: Color(0xFF57636C),
+                      ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  contentPadding:
+                      EdgeInsetsDirectional.fromSTEB(24, 24, 20, 24),
+                ),
+                style: Theme.of(context).textTheme.bodyText1?.apply(
+                      fontFamily: 'Outfit',
+                      color: Color(0xFF0F1113),
+                    ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+            child: GrowthronButton(
+              onPressed: () async {
+                if (emailAddressController!.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Email required!',
+                      ),
+                    ),
+                  );
+                  return;
+                }
+                await resetPassword(emailAddressController!.text);
+              },
+              text: 'Verify',
+              options: GrowthronButtonOptions(
+                width: 270,
+                height: 50,
+                color: Color(0xFF0F1113),
+                textStyle: Theme.of(context).textTheme.titleMedium?.apply(
+                      fontFamily: 'Outfit',
+                      color: Colors.white,
+                    ),
+                elevation: 3,
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
