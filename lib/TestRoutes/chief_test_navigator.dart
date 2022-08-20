48b0fd9675b3493920905c99f30ce0f8e9075a44
Abdulkabir Agboola <pro.kaiserlautern@gmail.com>
2022-08-20 15:11:07 +0100

    chief test navigator enabled
    (cherry picked from commit a62bd2478fd6a844a57792809bb9cfab4929ccf1)

---
+++ lib/TestRoutes/chief_test_navigator.dart
@@ -0,0 +1,63 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:growthclub/pages/chat.dart';
+import 'package:growthclub/pages/createTask.dart';
+import 'package:growthclub/pages/forgotPassword.dart';
+import 'package:growthclub/pages/loginIn.dart';
+import 'package:growthclub/pages/register.dart';
+import 'package:growthclub/pages/signInWithPhone.dart';
+
+import '../pages/mainScreen.dart';
+import '../pages/taskActivityLog.dart';
+import 'Routes/starter.dart';
+
+class TestNavigatorWidget extends StatelessWidget {
+  const TestNavigatorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<MapEntry> pageMapping = {
+      'Onboarding': OnboardingPage,
+      'MainScreen': MainScreenPage,
+      'Login': LoginPage,
+      'Register': RegisterPage,
+      'CreateTask': CreateTaskPage,
+      'CallSomeone': null,
+      'ActivityLog': TrackMyProgressStoryPage,
+      'Chat': ChatPageWidget,
+      'ForgotPassword': ForgotPasswordPage,
+      'PhoneSignIn': SignInWithPhonePage,
+    }.entries.toList();
+
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+      ),
+      itemCount: pageMapping.length,
+      itemBuilder: (BuildContext context, int index) {
+        final MapEntry mapEntry = pageMapping[index];
+        final String text = mapEntry.key;
+        final Widget pageRouting = mapEntry.value;
+
+        return InkWell(
+          onTap: () async {
+            // TODO: Reconfigure the code below to fit Navigator guidelines
+            await Navigator.of(context).push(pageRouting);
+          },
+          child: Card(
+            color: Colors.green,
+            child: AutoSizeText(
+              text,
+              style: const TextStyle(color: Colors.white, fontSize: 30),
+              minFontSize: 18,
+              maxLines: 4,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+class ChatPageWidget extends StatefulWidget {
+  const ChatPageWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ChatPageWidget> createState() => _ChatPageWidgetState();
+}
+
+class _ChatPageWidgetState extends State<ChatPageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Put a tangible thing in this
+    return Container();
+  }
+}
