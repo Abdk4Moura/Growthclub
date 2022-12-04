84cfd8b90f1e2d11676e411db5f0bcef745cd363
Abdulkabir Agboola <pro.kaiserlautern@gmail.com>
2022-12-04 17:41:43 +0100

    Redoing of lost progress. User Implemented once again, after the loss of data

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -1,117 +1,30 @@
-// @AppleStuff
-import 'dart:convert';
-import 'dart:math';
-
-import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:growthclub/dbschema.dart' as db;
 
-/// Generates a cryptographically secure random nonce, to be included in a
-/// credential request.
-// @AppleStuff
+class AuthModel extends ChangeNotifier {
+  bool get isSignedIn => FirebaseAuth.instance.currentUser != null;
 
-Future<UserCredential> signInWithGoogle() async {
-  // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth =
-      await googleUser?.authentication;
-
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
-
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithCredential(credential);
-}
-
-// @Apple Stuff
-String generateNonce([int length = 32]) {
-  const charset =
-      '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
-  final random = Random.secure();
-  return List.generate(length, (_) => charset[random.nextInt(charset.length)])
-      .join();
-}
-
-/// Returns the sha256 hash of [input] in hex notation.
-String sha256ofString(String input) {
-  final bytes = utf8.encode(input);
-  final digest = sha256.convert(bytes);
-  return digest.toString();
-}
-
-// Future<UserCredential> signInWithEmail() async {
-//   var acs = ActionCodeSettings(
-//     // URL you want to redirect back to. The domain (www.example.com) for this
-//     // URL must be whitelisted in the Firebase Console.
-//       url: 'https://www.example.com/finishSignUp?cartId=1234',
-//       // This must be true
-//       handleCodeInApp: true,
-//       iOSBundleId: 'com.example.ios',
-//       androidPackageName: 'com.example.android',
-//       // installIfNotAvailable
-//       androidInstallApp: true,
-//       // minimumVersion
-//       androidMinimumVersion: '12');
-//
-//   var emailAuth = 'someemail@domain.com';
-//   FirebaseAuth.instance.sendSignInLinkToEmail(
-//       email: emailAuth, actionCodeSettings: acs)
-//       .catchError((onError) =>
-//       print('Error sending email verification $onError'))
-//       .then((value) => print('Successfully sent email verification'));
-//
-//   return
-// }
-
-Future<UserCredential> signInWithApple() async {
-  // To prevent replay attacks with the credential returned from Apple, we
-  // include a nonce in the credential request. When signing in with
-  // Firebase, the nonce in the id token returned by Apple, is expected to
-  // match the sha256 hash of `rawNonce`.
-  final rawNonce = generateNonce();
-  final nonce = sha256ofString(rawNonce);
-
-  // Request credential for the currently signed in Apple account.
-  final appleCredential = await SignInWithApple.getAppleIDCredential(
-    scopes: [
-      AppleIDAuthorizationScopes.email,
-      AppleIDAuthorizationScopes.fullName,
-    ],
-    nonce: nonce,
-  );
-
-  // Create an `OAuthCredential` from the credential returned by Apple.
-  final oauthCredential = OAuthProvider("apple.com").credential(
-    idToken: appleCredential.identityToken,
-    rawNonce: rawNonce,
-  );
-
-  // Sign in the user with Firebase. If the nonce we generated earlier does
-  // not match the nonce in `appleCredential.identityToken`, sign in will fail.
-  return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
-}
+  db.User get user => User();
 
 // TODO: make signInWithFacebook work actually
-Future<UserCredential> signInWithFacebook() async {
-  // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth =
-      await googleUser?.authentication;
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
 
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    notifyListeners();
 
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithCredential(credential);
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 }
